174af3e545b1d7e11b353a345e2f6bb9f8c409b1
Bill Su <bill@humanlytics.co>
2019-04-12 11:17:20 -0400

    added custom studio logic, and logging

--- config.py
+++ config.py
@@ -4,3 +4,7 @@ import os
 DEBUG = True
 PORT = os.environ.get('PORT', 7000)
 HOST = os.environ.get('HOST', "0.0.0.0")
+
+#Mongodb Setting
+MONGO_URI = os.environ.get('MONGO_URI', 'mongodb://fnp:fnpadmin1@ds163764.mlab.com:63764/heroku_l189wfkq')
+MONGO_DB_NAME = os.environ.get('MONGO_DB_NAME', 'heroku_l189wfkq')
